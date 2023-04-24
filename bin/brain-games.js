061f673611b250929f0a85ef73243e66268c70c2
Nikolai Khalimendik <kolyakhalimendik@gmail.com>
2023-04-24 19:27:14 +0300

    step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import { greeting } from "/mnt/c/Users/mi/git-projects/frontend-project-44/src/cli.js";
-console.log("Welcome to the Brain Games!");
+import greeting from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 console.log(greeting());
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-  const name = readlineSync.question("May I have your name? ");
+const greeting = () => {
+  const name = readlineSync.question('May I have your name? ');
   const result = `Hello, ${name}!`;
   return result;
 };
+
+export default greeting;
